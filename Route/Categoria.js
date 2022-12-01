3b1bd25526503c93206604106ca005019a18704d
lucas1375 <lucasggg.striker@gmail.com>
2022-11-30 22:30:11 -0300

    atividade 3
    atividade 3

---
+++ Route/Categoria.js
@@ -0,0 +1,31 @@
+/*IMPORTA O PACOTE DO EXPRESS PARA O SCRIPT index.js*/
+const express = require('express');
+
+/*GERENCIADOR DE ROTAS DO EXPRESS*/
+const router = express.Router();
+
+
+//ROTA DE CADASTRO DE CATEGORIA
+//NOME(P1, P2, P3, P4){}
+router.post('/cadastrarCategoria', (req, res)=>{
+    res.send('ROTA DE CADASTRO DE CATEGORIA!');
+    console.log('Teste de NODEMON');
+});
+
+//ROTA DE LEITURA DE CATEGORIA
+router.get('/listarCategoria', (req, res)=>{
+    res.send('ROTA DE LISTAGEM DE CATEGORIA!');
+});
+
+//ROTA DE ALTERAÇÃO DE CATEGORIA
+router.put('/alterarCategoria', (req, res)=>{
+    res.send('ROTA DE ALTERAÇÃO DE CATEGORIA!');
+});
+
+//ROTA DE EXCLUSÃO DE CATEGORIA
+router.delete('/excluirCategoria', (req, res)=>{
+    res.send('ROTA DE EXCLUSÃO DE CATEGORIA!');
+});
+
+
+module.export
--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-//Boa noite professor,entao o meu projeto sera um livraria.
+/*IMPORTA O PACOTE DO EXPRESS PARA O SCRIPT index.js*/
+const express = require('express');
+
+/*TORNA O EXPRESS EXECUTÁVEL DENTRO DO SCRIPT inxdex.js
+ATRAVÉS DA CONSTANTE app
+*/
+const app = express();
+
+/** ROTAS DE CRUD DE CATEGORIA **/
+
+
+/** EXEMPLOS DE ROTA **/
+// app.get('/', function (req, res) {
+//     res.send('RESPOSTA DA ROTA RAIZ DA APLICAÇÃO');
+// });
+
+// app.get('/teste', function (req, res) {
+//     res.send('RESPOSTA DA ROTA TESTE DA APLICAÇÃO');
+// });
+
+/*
+CRIAR UM WEBSERVER CAPAZ DE RECEBER REQUISIÇÕES VIA
+HTTP
+PARAMETROS DO listem
+1 - PORTA LÓGICA (OBRIGATÓRIA)
+2 - CALLBACK (OPCIONAL)
+*/
+app.listen(3000, ()=>{
+    console.log('SERVIDOR RODANDO!');
+});
